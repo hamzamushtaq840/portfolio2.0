f8b290d5b22082ed7ac511fb37abbe7fd85fec89
Hamza Mushtaq <73603757+hamzamushtaq840@users.noreply.github.com>
2024-07-31 19:52:39 +0500

    feat: latest projects

--- src/components/projects/index.tsx
+++ src/components/projects/index.tsx
@@ -1,3 +1,4 @@
+import { cn } from "@/utils/utils";
 import Image from "next/image";
 import React from "react";
 
@@ -11,6 +12,34 @@ const LatestProjects = () => {
       <span className="mb-24 mt-2 text-lg font-medium leading-[32px] tracking-widest">
         REGONITIONS & ACCOIMPLISHMENTS
       </span>
+
+      <div className="flex flex-col gap-32">
+        {[1, 2, 3, 4].map((v, i) => {
+          return (
+            <div
+              className={cn("flex gap-14", i % 2 == 0 && "flex-row-reverse")}
+            >
+              <Image
+                src={"/portfolio.png"}
+                alt="pfp"
+                height={307}
+                width={460}
+              />
+              <div className="flex flex-col justify-between py-5">
+                <span className="flex self-start bg-textOrange px-4 py-2 text-lg font-bold leading-[32px]">
+                  ART DIRECTION
+                </span>
+                <span className="flex text-5xl font-bold leading-[56px]">
+                  Beautiful and advanced website interfaces
+                </span>
+                <span className="flex text-xl font-bold leading-[32px]">
+                  Read More
+                </span>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };

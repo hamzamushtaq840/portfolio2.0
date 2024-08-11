ba3bc48cd21a6db8b5eda7ceb21cf0853207044d
Hamza Mushtaq <73603757+hamzamushtaq840@users.noreply.github.com>
2024-08-11 17:37:30 +0500

    refactor: remove window resize implementation

--- src/components/model/index.tsx
+++ src/components/model/index.tsx
@@ -2,26 +2,11 @@
 
 import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-import { Suspense, useEffect, useState } from "react";
+import { Suspense } from "react";
 import Loader from "./Loader";
 import MeshComponent from "./MeshComponent";
 
 export function Model() {
-  const [enableZoom, setEnableZoom] = useState<boolean>(
-    window.innerWidth > 700,
-  );
-
-  useEffect(() => {
-    const handleResize = () => {
-      setEnableZoom(window.innerWidth > 700);
-    };
-
-    window.addEventListener("resize", handleResize);
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
   return (
     <div className="relative flex h-[45vh] w-full lg:h-[70vh] lg:w-[45%]">
       <Suspense fallback={<Loader />}>
@@ -29,7 +14,7 @@ export function Model() {
           camera={{ position: [0, 0.5, 5], fov: 45 }}
           className="h-2xl w-2xl"
         >
-          <OrbitControls enableZoom={enableZoom} />
+          <OrbitControls enableZoom={false} />
           <MeshComponent onLoad={() => {}} />
         </Canvas>
       </Suspense>

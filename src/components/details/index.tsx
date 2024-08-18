95dea203890b8a7fbed2d98ad705519241e7dbf7
Hamza Mushtaq <73603757+hamzamushtaq840@users.noreply.github.com>
2024-08-18 16:07:09 +0500

    fix: :fire: remove countup with no animation

--- src/components/details/index.tsx
+++ src/components/details/index.tsx
@@ -16,8 +16,7 @@ const Details = () => {
             )}
           >
             <span className="h-1/2 text-4xl font-bold lg:text-6xl">
-              {/* <CountUp enableScrollSpy={true} end={v.value} duration={2.5} />+ */}
-              {v.value}+
+              <CountUp enableScrollSpy={true} end={v.value} duration={2.5} />+
             </span>
             <span className="flex h-1/2 items-center px-2 text-center text-xs font-bold lg:text-lg">
               {v.label}
